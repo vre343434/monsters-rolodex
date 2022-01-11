e9094bcec9abdfe0537e9030637362ac33378028
vre343434 <vre343434@gmail.com>
2022-01-11 12:37:36 +0800

    adding files for github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+import "./search-box.stytle.css";
+
+export const SearchBox = props => (
+  <input
+    className='search-box'
+    type='search'
+    placeholder='search monsters'
+    onChange={props.onSearchChange}
+  />
+);
